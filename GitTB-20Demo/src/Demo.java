403755ef0f2322f537f39df4eff4abe771d709a4
amitcjc1 <61121166+amitcjc1@users.noreply.github.com>
2023-05-16 10:20:45 +0530

    by branch1
    Add new method name is m1. here print hello msg

--- GitTB-20Demo/src/Demo.java
+++ GitTB-20Demo/src/Demo.java
@@ -2,6 +2,13 @@
 public class Demo 
 {
 
+	public void m1()
+	{
+		System.out.println("Hello");
+	}
+	
+	
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Main Method Start");
